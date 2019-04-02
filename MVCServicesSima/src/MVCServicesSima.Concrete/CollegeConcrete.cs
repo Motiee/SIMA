85f701a573413a3ffbe27aba99c8097a0be837a3
Hesam Motiee <motiee@embedsys.ir>
2019-04-02 19:56:06 +0430

    Model, Interface and Concrete Projects added

---
+++ MVCServicesSima/src/MVCServicesSima.Concrete/CollegeConcrete.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVCServicesSima.Models;
+
+namespace MVCServicesSima.Concrete
+{
+    public class CollegeConcrete : MVCServicesSima.Interfaces.ICollege
+
+    {
+        public void Add(CollegeModel _college)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(CollegeModel _college)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CollegeModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CollegeModel GetByAutoID(long AutoID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CollegeModel _college)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVCServicesSima/src/MVCServicesSima.Models/CollegeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCServicesSima.Models
+{
+    public class CollegeModel
+    {
+        long autoID;
+        string collegeName;
+        string collegeCode;
+
+    }
+}
