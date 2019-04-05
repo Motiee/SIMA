1b77c78bc1fe7ea22fd57cdb06fed88e97d5ebd6
Hesam Motiee <motiee@embedsys.ir>
2019-04-05 11:00:10 +0430

    ICity Added

---
+++ MVCServicesSima/src/MVCServicesSima.Interfaces/ICity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCServicesSima.Interfaces
+{
+  public interface ICity
+    {
+        void Add(MVCServicesSima.Models.CityModel _City);
+        void Update(MVCServicesSima.Models.CityModel _City);
+        void Delete(MVCServicesSima.Models.CityModel _City);
+        MVCServicesSima.Models.CityModel GetByAreaId(int AreaId);
+        IEnumerable<MVCServicesSima.Models.CityModel> GetAll();
+    }
+}
--- MVCServicesSima/src/MVCServicesSima.Models/CityModel.cs
+++ MVCServicesSima/src/MVCServicesSima.Models/CityModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MVCServicesSima.Models
 {
-    class CityModel
+  public  class CityModel
     {
         int AreaId;
 
